d94f9f0551643638c27aa26e924f6003d4eec35e
Mark Lockett <mark@deliveryengine.net>
2019-01-17 09:59:57 +0800

    Config dependencies correctly

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],  # Optional
+    install_requires=['six>=1.12.0'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
@@ -165,4 +165,3 @@ setup(
     #    ],
     #},
 )
-
