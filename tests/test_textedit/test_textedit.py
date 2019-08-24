54dd11e9537a404015fd97170840b952c50e0b04
Mark Lockett <mark@deliveryengine.net>
2019-08-24 18:11:07 +0800

    Append to end of fragment created by insertion

--- tests/test_textedit/test_textedit.py
+++ tests/test_textedit/test_textedit.py
@@ -523,3 +523,54 @@ class TextEditTest(unittest.TestCase):
         assert textowner.text.get_fragment_at_index(0) == 0
         assert textowner.text.get_fragment_at_index(2) == 0
         assert textowner.text.get_fragment_at_index(3) == 0
+
+    def test_append_to_broken_fragment(self):
+        textowner = TestFieldTextEditOwner1()
+
+        dc1 = DocumentCollection(str(uuid.uuid4()))
+        dc1.register(TestFieldTextEditOwner1)
+        dc1.add_document_object(textowner)
+
+        textowner.text.insert(0, "abcdef")
+        textowner.text.insert(3, "z")
+        textowner.text.insert(7, "ghi")
+
+        self.assertEqual(textowner.text.get_text(), "abczdefghi")
+        self.assertEqual(len(textowner.text._listfragments), 3)
+        fragments = textowner.text._listfragments
+
+        assert fragments[0].text == "abc"
+        assert fragments[0].relative_to == ""
+        assert fragments[0].relative_start_pos == 0
+        assert fragments[0].has_been_split == True
+        assert fragments[0].internal_start_pos == 0
+
+        assert fragments[1].id != fragments[0].id
+        assert fragments[1].text == "z"
+        assert fragments[1].relative_to == fragments[0].id
+        assert fragments[1].relative_start_pos == 3
+        assert fragments[1].has_been_split == False
+        assert fragments[1].internal_start_pos == 0
+
+        assert fragments[2].id == fragments[0].id
+        assert fragments[2].text == "defghi"
+        assert fragments[2].relative_to == ""
+        assert fragments[2].relative_start_pos == 0
+        assert fragments[2].has_been_split == False
+        assert fragments[2].internal_start_pos == 3
+
+        assert textowner.text.get_fragment_to_append_to_by_index(0) == 0
+        assert textowner.text.get_fragment_to_append_to_by_index(2) == 0
+        assert textowner.text.get_fragment_to_append_to_by_index(3) == 0
+        assert textowner.text.get_fragment_to_append_to_by_index(4) == 1
+        assert textowner.text.get_fragment_to_append_to_by_index(5) == 2
+        assert textowner.text.get_fragment_to_append_to_by_index(7) == 2
+        assert textowner.text.get_fragment_to_append_to_by_index(10) == 2
+
+        assert textowner.text.get_fragment_at_index(0) == 0
+        assert textowner.text.get_fragment_at_index(2) == 0
+        assert textowner.text.get_fragment_at_index(3) == 1
+        assert textowner.text.get_fragment_at_index(4) == 2
+        assert textowner.text.get_fragment_at_index(5) == 2
+        assert textowner.text.get_fragment_at_index(6) == 2
+        assert textowner.text.get_fragment_at_index(9) == 2
