0f4e927f6c130c0e8b090eaa6d0af187f96398ad
Mark Lockett <mark@deliveryengine.net>
2019-08-31 14:29:44 +0800

    Insert two conflicting fragments

---
+++ tests/test_textedit/test_textedit_replication_conflict.py
@@ -0,0 +1,41 @@
+from __future__ import absolute_import, unicode_literals, print_function
+
+import unittest
+from historygraph import Document
+from historygraph import fields
+from ..common import DocumentCollection, TestFieldTextEditOwner1
+import uuid
+
+
+class TextEditTestReplication(unittest.TestCase):
+    # Test that the textedit works when replicated in complex scenarios
+    # where there are conflicting updates
+    def setUp(self):
+        self.dc1 = DocumentCollection(str(uuid.uuid4()))
+        self.dc1.register(TestFieldTextEditOwner1)
+        self.dc2 = DocumentCollection(str(uuid.uuid4()), master=self.dc1)
+        self.dc2.register(TestFieldTextEditOwner1)
+
+    def test_append_multi_conflicting_fragments(self):
+        textowner = TestFieldTextEditOwner1()
+
+        self.dc1.register(TestFieldTextEditOwner1)
+        self.dc1.add_document_object(textowner)
+
+        textowner.text.insert(0, "abc")
+
+        test2 = self.dc2.get_object_by_id(TestFieldTextEditOwner1.__name__,
+                                          textowner.id)
+
+        self.dc2.freeze_dc_comms()
+
+        textowner.text.insert(3, "def")
+
+        textowner.text.insert(3, "xyz")
+
+        self.dc2.unfreeze_dc_comms()
+
+        self.assertTrue(test2.text.get_text() == "abcdefxyz" or
+                        test2.text.get_text() == "abcxyzdef")
+
+        self.assertEqual(textowner.text.get_text(), test2.text.get_text())
